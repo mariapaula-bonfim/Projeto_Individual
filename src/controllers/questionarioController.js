931ad750875fbdb1e7ccfb134bf1c525fa587825
mariapaula-bonfim <maria.bonfim@sptech.school>
2026-05-19 22:15:19 -0300

    ajustes na inserção de dados

--- src/controllers/questionarioController.js
+++ src/controllers/questionarioController.js
@@ -1,32 +1,38 @@
 var questionarioModel = require("../models/questionarioModel");
 
-function cadastrar(req, res){
+function cadastrar(req, res) {
 
-    var faccao = req.body.faccaoServer;
-    var personagem = req.body.personagemServer;
-    var trilogia = req.body.trilogiaServer;
-    var filme = req.body.filmeServer;
+    var faccao     = req.body.faccaoServer;
+    var personagem = req.body.personagemServer;  // ainda vem do front como nome
+    var trilogia   = req.body.trilogiaServer;
+    var filme      = req.body.filmeServer;
     var quantidade = req.body.quantidadeServer;
-    var idUsuario = req.body.idUsuarioServer;
+    var lado       = req.body.ladoServer;        // campo novo
+    var elemento   = req.body.elementoServer;    // campo novo
+    var idUsuario  = req.body.idUsuarioServer;
 
-    questionarioModel.cadastrar(
-        faccao,
-        personagem,
-        trilogia,
-        filme,
-        quantidade,
-        idUsuario
-    )
+    // 1º: salva o questionário
+    questionarioModel.cadastrar(faccao, trilogia, filme, quantidade, lado, elemento, idUsuario)
 
-    .then(function(resultado){
-        res.json(resultado);
-    })
+        .then(function () {
+            // 2º: busca o id do personagem pelo nome escolhido
+            return questionarioModel.buscarIdPersonagem(personagem);
+        })
 
-    .catch(function(erro){
-        console.log(erro);
-        res.status(500).json(erro.sqlMessage);
-    });
+        .then(function (resultado) {
+            var idPersonagem = resultado[0].id_personagem;
+            // 3º: cria o vínculo N:N
+            return questionarioModel.vincularPersonagem(idUsuario, idPersonagem);
+        })
 
+        .then(function (resultado) {
+            res.json(resultado);
+        })
+
+        .catch(function (erro) {
+            console.log(erro);
+            res.status(500).json(erro.sqlMessage);
+        });
 }
 
 module.exports = {
--- src/models/questionarioModel.js
+++ src/models/questionarioModel.js
@@ -1,43 +1,58 @@
 var database = require("../database/config");
 
-function cadastrar(
-    faccao,
-    personagem,
-    trilogia,
-    filme,
-    quantidade,
-    idUsuario
-){
+// Salva o questionário (sem personagem_favorito — agora é relação N:N)
+function cadastrar(faccao, trilogia, filme, quantidade, lado, elemento, idUsuario) {
 
     var instrucaoSql = `
-
         INSERT INTO questionario
         (
             faccao_favorita,
-            personagem_favorito,
             trilogia_favorita,
             filme_favorito,
-            quantidade_assistidos,
+            qtd_filmes_assistidos,
+            lado_favorito,
+            elemento_favorito,
             id_usuario
         )
-
         VALUES
         (
             '${faccao}',
-            '${personagem}',
             '${trilogia}',
             '${filme}',
             ${quantidade},
+            '${lado}',
+            '${elemento}',
             ${idUsuario}
         );
-
     `;
 
-    console.log(instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Busca o id do personagem pelo nome
+function buscarIdPersonagem(nomePersonagem) {
+
+    var instrucaoSql = `
+        SELECT id_personagem FROM personagem
+        WHERE nome = '${nomePersonagem}';
+    `;
+
+    return database.executar(instrucaoSql);
+}
+
+// Cria o vínculo N:N entre usuário e personagem favorito
+function vincularPersonagem(idUsuario, idPersonagem) {
+
+    var instrucaoSql = `
+        INSERT INTO usuario_personagem (id_usuario, id_personagem)
+        VALUES (${idUsuario}, ${idPersonagem});
+    `;
 
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
-    cadastrar
+    cadastrar,
+    buscarIdPersonagem,
+    vincularPersonagem
 };
